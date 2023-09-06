43079da7357778ef0be0d1913d900958a3de7fc2
hackcharms <zaa78692@gmail.com>
2023-09-06 15:40:49 +0530

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   css: ["~/assets/css/index.scss"],
   modules: ["@nuxtjs/tailwindcss"],
