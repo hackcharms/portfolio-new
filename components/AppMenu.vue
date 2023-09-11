33b7d2c21df6b43755035e2dfd89b20e67e97312
hackcharms <zaa78692@gmail.com>
2023-09-11 12:58:52 +0530

    added hover effect

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -15,8 +15,8 @@
               <nuxt-link
                 :to="link.to"
                 :aria-lable="link.label"
-                class="relative uppercase text-3xl text-primary text-center font-bold px-10 py-2 block hover:border"
-                exact-active-class="border border-red"
+                class="relative uppercase text-3xl text-primary overflow-hidden text-center font-bold px-4 py-2 block hover:drop-shadow-lg opacity-effect custom-border custom-border-animate"
+                exact-active-class="drop-shadow-lg activeLink opacity-100"
               >
                 {{ link.title }}
               </nuxt-link>
@@ -29,14 +29,48 @@
 </template>
 
 <script>
-import { links } from '~/utils/lists'
+import { links } from "~/utils/lists";
 
 export default {
   data() {
     return {
       isOpen: false,
-      links
-    }
+      links,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.opacity-effect:not(.router-link-active){
+  --opacity-value:.8;
+  opacity: var(--opacity-value);
+  transition: opacity 1s;
+  &:hover{
+    --opacity-value:1;
   }
 }
-</script>
+.custom-border {
+  --x-translate-value: -110%;
+  --y-translate-value: 0%;
+  --border-color:transparent;
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    border-bottom: 8px solid var(--border-color);
+    translate: var(--x-translate-value) var(--y-translate-value);
+    transition: all 1s ease;
+  }
+}
+.custom-border-animate:hover::after{
+  --x-translate-value: 0%;
+  --border-color:blue;
+}
+.activeLink::after{
+  --x-translate-value: 0%;
+  --border-color:blue;
+}
+</style>
